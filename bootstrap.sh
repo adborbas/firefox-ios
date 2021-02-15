530e47124d34aaede639dd1c2f4ca36e70fe1ad9
Adam Borbas <adam.borbas@bitrise.io>
2021-02-15 11:40:59 +0100

    rvt exit

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,11 +16,11 @@
 getLocale()
 {
   echo "Getting locale..."
-  git clone https://github.com/boek/ios-l10n-scripts.git -b new_tool || exit 0
+  git clone https://github.com/boek/ios-l10n-scripts.git -b new_tool || exit 1
 
   echo "Creating firefoxios-l10n Git repo"
   rm -rf firefoxios-l10n
-  git clone --depth 1 https://github.com/mozilla-l10n/firefoxios-l10n firefoxios-l10n || exit 0
+  git clone --depth 1 https://github.com/mozilla-l10n/firefoxios-l10n firefoxios-l10n || exit 1
 
   ./ios-l10n-scripts/ios-l10n-tools --project-path Client.xcodeproj --l10n-project-path ./firefoxios-l10n --import
 }
